26f184421cadaa7691084b9d56a8b5503ea35c8d
kmakai <makaikwag@gmail.com>
2023-08-14 15:34:16 -0400

    Add project files.

---
+++ kmakai.MVC.RunningTracker/Models/RunModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kmakai.MVC.RunningTracker.Models;
+
+public class RunModel
+{
+    public int Id { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime Date { get; set; }
+    public double Distance { get; set; }
+
+    public TimeSpan Time { get; set; }
+}
